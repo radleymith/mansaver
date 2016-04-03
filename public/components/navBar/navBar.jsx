c97fa9b9ba15c6f9129052a093153b5bc48e1548
BradleySmith <bksmith23@gmail.com>
2016-04-03 13:44:16 -0400

    forgot to save a component

--- public/components/navBar/navBar.jsx
+++ public/components/navBar/navBar.jsx
@@ -4,61 +4,60 @@ import { Link } from 'react-router';
 import './navBar.less';
 import { NotAuthenticated, Authenticated, LoginLink, LogoutLink } from 'react-stormpath';
 
-const NavBar = React.createClass({
-    render: function () {
-        return (
-            <div id="nav-bar" className="pull-right">
-                <ul className="nav navbar-nav" role="nav">
-                    <li id="nav-about" className="nav-bar-item">
-                        <Link to="/" onlyActiveOnIndex>
-                            About
+const NavBar = () => {
+    return (
+        <div id="nav-bar" className="pull-right">
+            <ul className="nav navbar-nav" role="nav">
+                <li id="nav-about" className="nav-bar-item">
+                    <Link to="/" onlyActiveOnIndex>
+                        About
+                    </Link>
+                </li>
+                <li id="nav-what-to-expect" className="nav-bar-item">
+                    <Link to="/expect">
+                        What to Expect
+                    </Link>
+                </li>
+                <li id="nav-subscribe" className="nav-bar-item">
+                    <Link to="/subscribe">
+                        Subscribe
+                    </Link>
+                </li>
+                <li id="nav-pipe" className="nav-bar-item">
+                    <a>
+                        |
+                    </a>
+                </li>
+                <NotAuthenticated>
+                    <li id="nav-login" className="nav-bar-item">
+                        <LoginLink to="/login">
+                            Login
+                        </LoginLink>
+                    </li>
+                </NotAuthenticated>
+                <NotAuthenticated>
+                    <li id="nav-register" className="nav-bar-item">
+                        <Link to="/register">
+                            Register
                         </Link>
                     </li>
-                    <li id="nav-what-to-expect" className="nav-bar-item">
-                        <Link to="/expect">
-                            What to Expect
+                </NotAuthenticated>
+                <Authenticated>
+                    <li id="nav-profile" className="nav-bar-item">
+                        <Link to="/me">
+                            Profile 
                         </Link>
                     </li>
-                    <li id="nav-subscribe" className="nav-bar-item">
-                        <Link to="/subscribe">
-                            Subscribe
-                        </Link>
+                </Authenticated>
+                <Authenticated>
+                    <li id="nav-profile" className="nav-bar-item">
+                        <LogoutLink />
                     </li>
-                    <li id="nav-pipe" className="nav-bar-item">
-                        <a>
-                            |
-                        </a>
-                    </li>
-                    <NotAuthenticated>
-                        <li id="nav-login" className="nav-bar-item">
-                            <LoginLink to="/login">
-                                Login
-                            </LoginLink>
-                        </li>
-                    </NotAuthenticated>
-                    <NotAuthenticated>
-                        <li id="nav-register" className="nav-bar-item">
-                            <Link to="/register">
-                                Register
-                            </Link>
-                        </li>
-                    </NotAuthenticated>
-                    <Authenticated>
-                        <li id="nav-profile" className="nav-bar-item">
-                            <Link to="/me">
-                                Profile 
-                            </Link>
-                        </li>
-                    </Authenticated>
-                    <Authenticated>
-                        <li id="nav-profile" className="nav-bar-item">
-                            <LogoutLink />
-                        </li>
-                    </Authenticated>
-                </ul>
-            </div>         
-        );
-    }
-});
+                </Authenticated>
+            </ul>
+        </div>         
+    );
+};
+
 
 export default NavBar;
